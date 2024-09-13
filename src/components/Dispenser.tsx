3f9b296c0c00807d0ad675673a841ad228fe5a2a
Nicolas Wormser <nicolas@codewithflair.org>
2024-09-13 16:56:04 +0200

    Add print button

--- src/components/Dispenser.tsx
+++ src/components/Dispenser.tsx
@@ -11,34 +11,40 @@ function Dispenser({ setTicketList }: DispenserProps) {
   // Auto-print counter, resets at every print
   const [autoPrintCountdown, setAutoPrintCountdown] = useState(PRINT_PERIOD)
 
+  const printTicket = () => {
+    setTicketList((old) => [...old, new TicketItem()])
+    setAutoPrintCountdown(PRINT_PERIOD)
+  }
   useEffect(() => {
     const timer = setInterval(() => {
       if (autoPrintCountdown === 0) {
-        setTicketList((old) => [...old, new TicketItem()])
-        setAutoPrintCountdown(PRINT_PERIOD)
+        printTicket()
       } else {
         setAutoPrintCountdown((prevTime) => prevTime - 1)
       }
     }, 1000)
 
     return () => clearInterval(timer)
-  }, [autoPrintCountdown, setTicketList])
+  }, [autoPrintCountdown, printTicket, setTicketList])
 
   return (
     <div
       style={{
         width: "200px",
-        height: "200px",
+        height: "300px",
         background: "rgba(0,0,0,.6)",
         borderRadius: "1rem",
-        padding: "1rem",
+        padding: "2rem",
         fontSize: "2rem",
-        display: "grid",
-        placeContent: "center",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        alignContent: "center",
       }}
     >
       Printing new ticket in <br />
       {autoPrintCountdown}
+      <button onClick={printTicket}>PRINT</button>
     </div>
   )
 }
